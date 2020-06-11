d0ad9e7ad626f8f68b474810260883ce9f6aace3
Michalina Kmieciak <michalinaasd@gmail.com>
2020-06-11 22:52:37 +0200

    lab8

---
+++ lab8/lab8/Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using lab8.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab8.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Name { get; set; }
+        public ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ lab8/lab8/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab8.Models
+{
+    public class Song
+    {
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Name{ get; set; }
+        [Required(ErrorMessage = "Artist field is required!")]
+        [StringLength(50, ErrorMessage = "Maximal length of the name of an artist is 50 characters!")]
+        public string Artist { get; set; }
+        public int GenreId { get; set; }
+        public int Id { get; set; }
+    }
+}
